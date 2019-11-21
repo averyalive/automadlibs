d4d3a7d2ed3d467c091c73e776f571de2d289eaf
Avery Koranda <koran023@morris.umn.edu>
2019-11-21 17:59:01 -0600

    Re-create material module for Angular modules

---
+++ client/src/app/material.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  MatButtonModule,
+} from '@angular/material';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+  ],
+  exports: [
+    MatButtonModule
+  ]
+})
+export class MaterialModule { }
