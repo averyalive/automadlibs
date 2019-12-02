bb7cf532f4c3a5073960146db970c823f1beb68f
ananyateklu <teklu009@morris.umn.edu>
2019-12-01 20:02:13 -0600

    Create an index for the table words

--- data/database_design/generate_tables.sql
+++ data/database_design/generate_tables.sql
@@ -1,20 +1,23 @@
 -- see ER diagram at https://drive.google.com/file/d/1oEBwBVsq94VZssCmFlKziv3_6xqUpn0c/view?usp=sharing
 
 -- this table should also probably be indexed
-CREATE OR REPLACE TABLE words (
+CREATE TABLE words (
     id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     spelling VARCHAR(100),
     type VARCHAR(100)
 );
 
-CREATE OR REPLACE TABLE madlibs (
+-- create index on the column spelling 
+CREATE INDEX ORDER ON automadlibs.words(spelling);
+
+CREATE TABLE madlibs (
     id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    contents VARCHAR,
+    contents json
     -- reference the words it uses?
     -- reference the template it used?
 );
 
-CREATE OR REPLACE TABLE templates (
+CREATE TABLE templates (
     id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     contents BLOB
     -- reference the madlibs generated from this template?
@@ -25,3 +28,11 @@ CREATE OR REPLACE TABLE templates (
 LOAD XML LOCAL INFILE '../dictionary/full/dictionary.xml'
 INTO TABLE automadlibs.words
 ROWS IDENTIFIED BY '<word>';
+
+
+--insert mabslibs from madlib-1.json
+-- assuming mysql client is being run from this file's directory.
+--LOAD DATA INFILE "../madlibs/madlib-1.json"
+--INTO TABLE automadlibs.madlibs
+--FIELDS TERMINATED BY '\0' ESCAPED BY ''
+--LINES TERMINATED BY '\0';
