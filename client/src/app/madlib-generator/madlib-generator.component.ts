5236d02fdd318a48b8c3457423595e4233906edb
Avery Koranda <koran023@morris.umn.edu>
2019-12-03 14:11:46 -0600

    Refactor generator component to display some data

--- client/src/app/madlib-generator/madlib-generator.component.ts
+++ client/src/app/madlib-generator/madlib-generator.component.ts
@@ -8,12 +8,14 @@ import { ServerService } from '../server.service';
 })
 export class MadlibGeneratorComponent implements OnChanges {
   @Input() selectedTemplate;
+  madlibHtml;
 
   constructor(private serverService: ServerService) { }
 
   ngOnChanges() {
     this.serverService.createMadlib(this.selectedTemplate).then(data => {
       console.log(data);
+      this.madlibHtml = '<p>' + JSON.stringify(data) + '</p>';
     },
     err => {
       console.log(err);
