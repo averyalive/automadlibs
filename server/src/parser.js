e17d3fca25cb51ebfb18fb504ba5b887fa6f287c
Avery Koranda <koran023@morris.umn.edu>
2019-12-04 23:23:48 -0600

    Resolve merge conflict with parser.js

--- server/src/parser.js
+++ server/src/parser.js
@@ -1,27 +1,5 @@
 const {promisify} = require('util');
 
-<<<<<<< HEAD
-function init(db) {
-    this.db = db;
-}
-
-function parse (s) {
-    s = s.replace(/$noun/, getWord('nouns'));
-    s = s.replace(/$verb/, getWord('verbs'));
-    s = s.replace(/$adjective/, getWord('adjectives'));
-    s = s.replace(/$adverb/, getWord('adverbs'));
-}
-
-function getWord(type) {
-    this.db.query(`SELECT * FROM ${type} LIMIT 1`,
-    (err, data) => {
-        if (err) {
-            console.log(err);
-        } else {
-            return data;
-        }
-    });
-=======
 class Parser {
     constructor(db) {
         this.db = db;
@@ -58,6 +36,5 @@ class Parser {
     getWord(type) {
         return this.query(`SELECT * FROM ${type}`);
     }
->>>>>>> 582b4dce368a9eed198a8d60d0102d61265fa1bf
 }
 module.exports = Parser;
